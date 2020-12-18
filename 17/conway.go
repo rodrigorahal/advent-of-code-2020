d794a47b709ecfc766fdffda84eae4359c9be2d7
rodrigorahal <rodrigo@livup.com.br>
2020-12-18 00:51:22 -0300

    Add day 17 4D

--- 17/conway.go
+++ 17/conway.go
@@ -7,6 +7,21 @@ import (
 	"os"
 )
 
+func hyperStatus(hyperspace [][][][]string, hyper, depth, row, col int) string {
+	var h, z, height, width = len(hyperspace), len(hyperspace[0]), len(hyperspace[0][0]), len(hyperspace[0][0][0])
+	var state string
+
+	if (hyper < 0 || hyper >= h) ||
+		(depth < 0 || depth >= z) ||
+		(row < 0 || row >= height) ||
+		(col < 0 || col >= width) {
+		state = "."
+	} else {
+		state = hyperspace[hyper][depth][row][col]
+	}
+	return state
+}
+
 func status(space [][][]string, depth, row, col int) string {
 	var z, height, width = len(space), len(space[0]), len(space[0][0])
 	var state string
@@ -19,6 +34,43 @@ func status(space [][][]string, depth, row, col int) string {
 		state = space[depth][row][col]
 	}
 	return state
+}
+
+func hyperNewState(hyperspace [][][][]string, hyper, depth, row, col int) string {
+	currentState := hyperStatus(hyperspace, hyper, depth, row, col)
+	activeNeighbors := 0
+
+	for w := hyper - 1; w <= hyper+1; w++ {
+		for k := depth - 1; k <= depth+1; k++ {
+			for j := row - 1; j <= row+1; j++ {
+				for i := col - 1; i <= col+1; i++ {
+					if (w == hyper) && (k == depth) && (j == row) && (i == col) {
+						continue
+					}
+					if hyperStatus(hyperspace, w, k, j, i) == "#" {
+						activeNeighbors++
+					}
+				}
+			}
+		}
+	}
+
+	var updatedState string
+	switch currentState {
+	case "#":
+		if activeNeighbors == 2 || activeNeighbors == 3 {
+			updatedState = currentState
+		} else {
+			updatedState = "."
+		}
+	case ".":
+		if activeNeighbors == 3 {
+			updatedState = "#"
+		} else {
+			updatedState = currentState
+		}
+	}
+	return updatedState
 }
 
 func newState(space [][][]string, depth, row, col int) string {
@@ -56,6 +108,20 @@ func newState(space [][][]string, depth, row, col int) string {
 	return updatedState
 }
 
+func emptySpace(depth, height, width int) [][][]string {
+	result := make([][][]string, depth)
+	for k := 0; k < depth; k++ {
+		result[k] = make([][]string, height)
+		for j := 0; j < height; j++ {
+			result[k][j] = make([]string, width)
+			for i := 0; i < width; i++ {
+				result[k][j][i] = "."
+			}
+		}
+	}
+	return result
+}
+
 func emptyGrid(height, width int) [][]string {
 	result := make([][]string, height)
 	for i := 0; i < height; i++ {
@@ -73,6 +139,39 @@ func emptyRow(width int) []string {
 		result[i] = "."
 	}
 	return result
+}
+
+func hyperPadding(hyperspace [][][][]string) [][][][]string {
+	var depth, height, width = len(hyperspace[0]), len(hyperspace[0][0]), len(hyperspace[0][0][0])
+
+	// Add extra rows and cols
+	for k, space := range hyperspace {
+		for j, grid := range space {
+			for i := range grid {
+				hyperspace[k][j][i] = append(hyperspace[k][j][i][:1], hyperspace[k][j][i][0:]...)
+				hyperspace[k][j][i][0] = "."
+				hyperspace[k][j][i] = append(hyperspace[k][j][i], ".")
+			}
+			hyperspace[k][j] = append(hyperspace[k][j][:1], hyperspace[k][j][0:]...)
+			hyperspace[k][j][0] = emptyRow(width + 2)
+			hyperspace[k][j] = append(hyperspace[k][j], emptyRow(width+2))
+		}
+	}
+
+	// Add extra grids
+	for k := range hyperspace {
+		hyperspace[k] = append(hyperspace[k][:1], hyperspace[k][0:]...)
+		hyperspace[k][0] = emptyGrid(height+2, width+2)
+		hyperspace[k] = append(hyperspace[k], emptyGrid(height+2, width+2))
+
+	}
+
+	// Add extra spaces
+	hyperspace = append(hyperspace[:1], hyperspace[0:]...)
+	hyperspace[0] = emptySpace(depth+2, height+2, width+2)
+	hyperspace = append(hyperspace, emptySpace(depth+2, height+2, width+2))
+
+	return hyperspace
 }
 
 func padding(space [][][]string) [][][]string {
@@ -98,6 +197,28 @@ func padding(space [][][]string) [][][]string {
 	return space
 }
 
+func hyperRun(hyperspace [][][][]string) int {
+	current := hyperspace
+	for i := 1; i <= 6; i++ {
+		current = hyperStep(current)
+	}
+
+	active := 0
+	for _, space := range current {
+		for _, grid := range space {
+			for _, row := range grid {
+				for _, col := range row {
+					if col == "#" {
+						active++
+					}
+				}
+			}
+		}
+	}
+
+	return active
+}
+
 func run(space [][][]string) int {
 	current := space
 	for i := 1; i <= 6; i++ {
@@ -118,6 +239,24 @@ func run(space [][][]string) int {
 	return active
 }
 
+func hyperStep(hyperspace [][][][]string) [][][][]string {
+	prev := hyperdeepercopy(hyperspace)
+
+	prev = hyperPadding(prev)
+	hyperspace = hyperPadding(hyperspace)
+
+	for w, space := range hyperspace {
+		for z, grid := range space {
+			for j, row := range grid {
+				for i := range row {
+					hyperspace[w][z][j][i] = hyperNewState(prev, w, z, j, i)
+				}
+			}
+		}
+	}
+	return hyperspace
+}
+
 func step(space [][][]string) [][][]string {
 	prev := deepercopy(space)
 
@@ -135,6 +274,25 @@ func step(space [][][]string) [][][]string {
 	return space
 }
 
+func hyperRead(r io.Reader) [][][][]string {
+	var hyperspace [][][][]string
+	var space [][][]string
+	var grid [][]string
+
+	scanner := bufio.NewScanner(r)
+	i := 0
+	for scanner.Scan() {
+		grid = append(grid, make([]string, 0))
+		row := scanner.Text()
+		for _, column := range row {
+			grid[i] = append(grid[i], string(column))
+		}
+		i++
+	}
+	space = append(space, grid)
+	return append(hyperspace, space)
+}
+
 func read(r io.Reader) [][][]string {
 	var space [][][]string
 	var grid [][]string
@@ -150,6 +308,15 @@ func read(r io.Reader) [][][]string {
 		i++
 	}
 	return append(space, grid)
+}
+
+func hyperdeepercopy(hyperspace [][][][]string) [][][][]string {
+	duplicate := make([][][][]string, len(hyperspace))
+	for i := range hyperspace {
+		duplicateSpace := deepercopy(hyperspace[i])
+		duplicate[i] = duplicateSpace
+	}
+	return duplicate
 }
 
 func deepercopy(space [][][]string) [][][]string {
@@ -186,13 +353,12 @@ func printGrid(grid [][]string) {
 
 func main() {
 	file, _ := os.Open("input.txt")
-	space := read(file)
 	// space[depth][row][col]
-	printSpace(space)
-
-	// n := step(space)
-	// fmt.Println("new:")
-	// printSpace(n)
+	space := read(file)
 	fmt.Println(run(space))
 
+	file, _ = os.Open("input.txt")
+	// hyperspace[hyper][depth][row][col]
+	hyperspace := hyperRead(file)
+	fmt.Println(hyperRun(hyperspace))
 }
